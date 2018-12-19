818d9b79b838fce376f53245dc020795662d7ee2
Even Solbraa <esol@equinor.com>
2018-12-19 15:09:00 +0100

    updated path file

--- pathNeqSim.m
+++ pathNeqSim.m
@@ -1,28 +1,31 @@
-pathToJavaJar = 'C:\app\neqsimMatlab\ext\';
-pathstring = 'C:\app\neqsimMatlab\';
+pathstring = 'C:\Users\esol\OneDrive - Statoil ASA\programming\neqsimmatlab\';
 
 addpath(strcat(pathstring));
 addpath(strcat(pathstring, 'example'));
 addpath(strcat(pathstring, 'lib'));
+librarypath = strcat(pathstring, 'ext\');
+databasepath = strcat(pathstring, 'database\');
 
-tempClassPath = javaclasspath;
-if isempty(tempClassPath)
-    cellarraywithpaths=({});
-    rootpath = pathToJavaJar;
-    a=ls([rootpath '*.jar']);
-    for i = 1:length(a(:,:))
-        cellarraywithpaths{end+1}=[rootpath a(i,:)];
-    end
+cellarraywithpaths=({});
+
+if(isempty(javaclasspath))
+    jarFiles=ls([librarypath '*.jar']);
+        for i = 1:length(jarFiles(:,1))
+            cellarraywithpaths{end+1}=[librarypath jarFiles(i,:)];
+        end
     javaclasspath(cellarraywithpaths);
 end
-neqsim.util.database.NeqSimDataBase.setDataBaseType("MSAccessUCanAccess");
+
+neqsim.util.database.NeqSimDataBase.setDataBaseType("mySQL");
+neqsim.util.database.NeqSimDataBase.setConnectionString("jdbc:mysql://tr-w33:3306/neqsimthermodatabase");
+
+%neqsim.util.database.NeqSimDataBase.setDataBaseType("mySQLNTNU");
+neqsim.util.database.NeqSimDataBase.setConnectionString("jdbc:mysql://tr-w33:3306/neqsimthermodatabase");
+
 %neqsim.util.database.NeqSimDataBase.setDataBaseType("MSAccessUCanAccess");
-%alternative use mysql database (if NeqSim MySql database available on network)
-%NeqSimDataBase.dataBaseType = 'mySQL'; 
-NeqSimDataBase.dataBasePathMSAccess = 'C:/programming/NeqSimSourceCode/java/neqsim/data/NeqSimDatabase.mdb';
+%NeqSimDataBase.dataBasePathMSAccess = strcat(databasepath, 'NeqSimDatabase.mdb');
 
 if ~exist('processOperations')
     global processOperations
     processOperations = neqsim.processSimulation.processSystem.ProcessSystem;
 end % end added
-
