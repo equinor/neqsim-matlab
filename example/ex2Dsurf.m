7f35bb9f1a35309044fc3c72beb6f55a22a690fb
STATOIL-NET\ESOL <esol@equinor.com>
2019-03-11 13:53:42 +0100

    update of neqsim library to version 1.0.

--- example/ex2Dsurf.m
+++ example/ex2Dsurf.m
@@ -1,15 +1,16 @@
 clear all;
 test = neqsim.thermo.system.SystemSrkEos();
 test.addComponent('methane', 10.0);
-test.addComponent('water', 10.0);
+test.addComponent('n-heptane', 12.0);
+test.addComponent('nC10', 1.0);
 test.setMixingRule(2);
 
 i=0;
-for temperature = [270:2:290]
+for temperature = [270:2:590]
     i=i+1;
     j=0;
     temp(i) = temperature;
-    for pressure = [1:5:100]
+    for pressure = [1:2:300]
         j = j+1;
         pres(j) = pressure;
         test.setTemperature(temperature)
