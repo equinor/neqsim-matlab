d8a89c67a11ce88960d9d56c505988b05b6d5898
Even Solbraa <esol@equinor.com>
2019-03-25 15:37:10 +0100

    updates mercury

---
+++ example/mercury.m
@@ -0,0 +1,8 @@
+fluid1 = neqsim.thermo.system.SystemSrkEos(288.15, 2.0);
+fluid1.addComponent("methane", 91.0);
+fluid1.addComponent("c-hexane", 10.0);
+fluid1.addComponent("mercury", 1.0);
+fluid1.setMixingRule(2);
+fluid1.setMultiPhaseCheck(true);
+
+TPflash(fluid1)
