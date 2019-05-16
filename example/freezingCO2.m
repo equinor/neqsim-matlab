e7b5b1f981278c8e67766b8357f116cf70ceca9b
Even Solbraa <esol@equinor.com>
2019-05-16 17:03:44 +0200

    added freezingCO2 script

---
+++ example/freezingCO2.m
@@ -0,0 +1,8 @@
+system1 = thermo('srk', 250.0, 20.0); %  EoS / Temperature [Kelvin] / Pressure [bara]
+system1.addComponent('methane', 90.0); 
+system1.addComponent('CO2', 10.0);
+system1.setMixingRule('classic');
+system1.setSolidPhaseCheck('CO2');
+freezt(system1);
+system1
+disp(['freezing temperature ',num2str(system1.getTemperature()-273.15)])
--- example/testUMRPhaseEnvelope.m
+++ example/testUMRPhaseEnvelope.m
@@ -1,5 +1,5 @@
 fluid1 = thermo('UMR-PRU-EoS',273.15+55, 25.0);
-fluid1.addComponent('methane', 6.0);
+fluid1.addComponent('ethane', 26.0);
 fluid1.addComponent('n-hexane', 44.5);
 fluid1.createDatabase(1);
 fluid1.setMixingRule('UNIFAC_UMRPRU'); 
