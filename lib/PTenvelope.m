9c7f18e46ad1708d4267121e82a45f2c7bccc060
Even Solbraa <esol@equinor.com>
2019-08-23 08:11:31 +0200

    chaged call to phase envelope function

--- lib/PTenvelope.m
+++ lib/PTenvelope.m
@@ -2,7 +2,7 @@ function testFlash = PTenvelope(testSystem)
     testSystem.init(0)
     testSystem.init(1)
     testFlash = neqsim.thermodynamicOperations.ThermodynamicOperations(testSystem);
-    testFlash.calcPTphaseEnvelope(1);
+    testFlash.calcPTphaseEnvelope();
     testFlash.displayResult();
     return;
      
