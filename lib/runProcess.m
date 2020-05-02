48c7a1dbedca37707515966184e4eece3d4bb572
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2020-05-02 14:42:43 +0200

    feat: added missing wrapper function for processOperation.run (#33)

---
+++ lib/runProcess.m
@@ -0,0 +1,12 @@
+function runProcess()
+% Run Process operation simulation
+% function runProcess()
+%
+% DESCRIPTION:
+% Run processOperations simulation
+% Wrapper for processOperations.run() in java.
+
+global processOperations
+
+processOperations.run();
+end
