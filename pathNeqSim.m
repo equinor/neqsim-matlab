556d41b0c9f69f28b57a8f3d40f4cd0f905d77ce
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2021-06-03 12:16:13 +0200

    docfix

--- pathNeqSim.m
+++ pathNeqSim.m
@@ -1,12 +1,12 @@
 function pathNeqSim(baseFol)
-%
+% Add NeqSim funtions to path, setup database and call resetProcessOperations
 % function pathNeqSim(baseFol)
 %
 % OPTIONAL INPUT:
-%  - baseFol
+%  - baseFol - Overwrite Neqsim base folder.
 %
 % DESCRIPTION:
-%
+% Add NeqSim jar to path, setup database and call resetProcessOperations
 
 appDataName = 'NeqSimLoaded';
 if ~isappdata(0,appDataName) || ~getappdata(0,appDataName)
@@ -16,11 +16,9 @@ if ~isappdata(0,appDataName) || ~getappdata(0,appDataName)
         error('Input baseFol does not exist');
     end
     addpath(baseFol);
-    % addpath(fullfile(baseFol,'example'));
     addpath(fullfile(baseFol,'lib'));
     
     librarypath = fullfile(baseFol,'ext');
-    % databasepath = fullfile(baseFol, 'database');
     
     jarFiles = cellstr(ls(fullfile(librarypath,'*.jar')));
     for k = numel(jarFiles):-1:1
@@ -30,25 +28,6 @@ if ~isappdata(0,appDataName) || ~getappdata(0,appDataName)
             javaclasspath(currJar);
         end
     end
-    %
-    %     import neqsim.thermo.*;
-    %     import neqsim.thermo.system.*;
-    %     import neqsim.PVTsimulation.simulation.*;
-    %     import neqsim.thermodynamicOperations.*;
-    %     import neqsim.processSimulation.measurementDevice.*;
-    %     import neqsim.processSimulation.controllerDevice.*;
-    %     import neqsim.processSimulation.processSystem.*;
-    %     import neqsim.processSimulation.processEquipment.separator.*;
-    %     import neqsim.processSimulation.processEquipment.*;
-    %     import neqsim.processSimulation.processEquipment.mixer.*;
-    %     import neqsim.processSimulation.processEquipment.stream.*;
-    %     import neqsim.processSimulation.processEquipment.heatExchanger.*;
-    %     import neqsim.processSimulation.processEquipment.compressor.*;
-    %     import neqsim.processSimulation.processEquipment.valve.*;
-    %     import neqsim.processSimulation.processEquipment.splitter.*;
-    %     import neqsim.processSimulation.processEquipment.absorber.*;
-    %     import neqsim.processSimulation.processEquipment.absorber.*;
-    %     import neqsim.processSimulation.processEquipment.util.*;
     
     neqsim.util.database.NeqSimDataBase.setDataBaseType("Derby");
     neqsim.util.database.NeqSimDataBase.setConnectionString("jdbc:derby:classpath:data/neqsimthermodatabase");
