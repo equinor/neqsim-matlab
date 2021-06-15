8ce2d3fdb434da3b4f414352a591b78ecdd95c54
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2021-06-16 00:07:20 +0200

    update of example (#39)
    * updated neqsim library, with function getResultTable and calcResultTable
    * Update testNewLib.m to show functionality
    
    * feat: update function show to use new functionality, return data table if requested and include column names to table
    
    Co-authored-by: Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>

--- lib/show.m
+++ lib/show.m
@@ -1,12 +1,13 @@
-function hFig = show(obj)
+function [hFig,T] = show(obj)
 % Show summary table of NEQSIM object if available
-% function hFig = show(obj)
+% function [hFig,T] = show(obj)
 %
 % INPUT:
 %  - thermoSystem - NEQSIM object
 %
 % OUTPUT:
 %  - hFig         - Figure handle if display is a matlab GUI
+%  - T            - Table data displayed if available.
 %
 % DESCRIPTION:
 % Show summary table of NEQSIM object if available
@@ -15,21 +16,43 @@ function hFig = show(obj)
 % show(thermoSystem);
 % show(valve)
 
-try
-    if isa(obj,'neqsim.thermo.system.SystemInterface')
-        C = cell(obj.createTable("test"));
-        T = cell2table(C);
-        if nargout > 0
-            hFig = uitableplot(T);
-        end
-        return
+if isjava(obj)
+    objClass = class(obj);
+    if ~startsWith(objClass,'neqsim')
+        error('neqsim:show:noJavaObject','Input obj is not a NeqSim object');
     end
+else
+    error('neqsim:show:noJavaObject','Input obj is not a java object, and then not a NeqSim object');
+end
+
+functions = methods(obj);
+
+try
+    if ismember('getResultTable',functions)
+        C = cell(obj.getResultTable());
+    elseif ismember('calcResultTable',functions)
+        C = cell(obj.calcResultTable());
+    elseif isa(obj,'neqsim.thermo.system.SystemInterface')
+        C = cell(obj.createTable("test"));
+    end
+    T = cell2table(C(2:end,:));
+    indHasColumnName = ~cellfun(@isempty,C(1,:));
+    T.Properties.VariableNames(indHasColumnName) = C(1,indHasColumnName);
+    T.Properties.VariableNames{1} = 'Property';
+    T.Properties.VariableNames{end} = 'Unit';
+    
+    if isfunction('uitableplot')
+        hFig = uitableplot(T);
+    else
+        hFig = gobjects(1);
+    end
+    return
 catch
 end
 
-if ismember('display',methods(obj))
+if ismember('display',functions)
     obj.display();
-elseif ismember('displayResult',methods(obj))
+elseif ismember('displayResult',functions)
     obj.displayResult();
 else
     warning('show:notPossibleToShow','No display function available for object of type %s',class(obj));
@@ -37,4 +60,5 @@ end
 
 if nargout > 0
     hFig = gobjects(1);
+    T = table.empty;
 end
