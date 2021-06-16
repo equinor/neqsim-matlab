8235d89eff1ef676799139f296fae17bb4cd55d1
EvenSol <esolbraa@gmail.com>
2021-06-16 09:23:47 +0200

    updated restAPI call
    A working NeqSim restAPI call

--- example/testRestAPI.m
+++ example/testRestAPI.m
@@ -21,12 +21,9 @@ data = struct('feedGasFlowRate', (117000.0+90000.0)*24.0/1.0e6, ...
   'stageEfficiencyStripper', 0.5, ...
   'numberOfEquilibriumStagesStripper', 4, ...
   'UAvalueLeanRichTEGHeatExchanger', 16100.0);
-% headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Ocp-Apim-Subscription-Key':'xxxxxxxxxxxx'}
+
 % Ocp-Apim-Subscription-Key available from: https://api-test.portal.equinor.com/products/neqsim-live
-
-% response = requests.post('https://jsv-neqsim-live-api.playground.radix.equinor.com/jsv/dehydSim', data=json.dumps(data), headers=headers)
-% response = requests.post('http://localhost:8080/jsv/dehydSim', data=json.dumps(data), params=params, headers=headers) #used for running local
-
-% Get token: https://se.mathworks.com/matlabcentral/answers/737172-how-can-i-obtain-azure-ad-tokens-in-matlab-r2020b
-options = weboptions('KeyName','Ocp-Apim-Subscription-Key','KeyValue','xxx');
-response = webwrite('https://api-test.gateway.equinor.com/neqsim/jsv/dehydSim',jsonencode(data),options)
+options = weboptions('KeyName','Ocp-Apim-Subscription-Key','KeyValue','xxxxxx');
+options.Timeout = 300.0
+options.HeaderFields = matlab.net.http.HeaderField('Content-Type','application/json');
+response = webwrite('https://api-test.gateway.equinor.com/neqsim/jsv/dehydSim', jsonencode(data),options)
