60685b015ba23cb059d76be92a20ee98e9602ea6
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-02-14 10:58:07 +0100

    wip

--- lib/show.m
+++ lib/show.m
@@ -1,16 +1,16 @@
 function [hFig,T] = show(obj)
-% Show summary table of NEQSIM object if available
+% Show summary table of NeqSim object if available
 % function [hFig,T] = show(obj)
 %
 % INPUT:
-%  - thermoSystem - NEQSIM object
+%  - obj  - NeqSim object
 %
 % OUTPUT:
-%  - hFig         - Figure handle if display is a matlab GUI
-%  - T            - Table data displayed if available.
+%  - hFig - Figure handle if display is a matlab GUI
+%  - T    - Table data displayed if available.
 %
 % DESCRIPTION:
-% Show summary table of NEQSIM object if available
+% Show summary table of NeqSim object if available
 %
 % EXAMPLE:
 % show(thermoSystem);
@@ -32,22 +32,30 @@ try
         C = cell(obj.getResultTable());
     elseif ismember('calcResultTable',functions)
         C = cell(obj.calcResultTable());
-    elseif isa(obj,'neqsim.thermo.system.SystemInterface')
+    elseif isa(obj,'neqsim.thermo.system.SystemInterface') || ismember('createTable',functions)
         C = cell(obj.createTable("test"));
     end
+    
+    try
+        indEmpty = all(cellfun(@isempty,C),1);
+        C(:,indEmpty) = [];
+    catch
+    end
+
     T = cell2table(C(2:end,:));
     indHasColumnName = ~cellfun(@isempty,C(1,:));
     T.Properties.VariableNames(indHasColumnName) = C(1,indHasColumnName);
     T.Properties.VariableNames{1} = 'Property';
     T.Properties.VariableNames{end} = 'Unit';
-    
+
     if isfunction('uitableplot')
         hFig = uitableplot(T);
     else
         hFig = gobjects(1);
     end
     return
-catch
+catch ME
+    disp(ME.getReport)
 end
 
 if ismember('display',functions)
